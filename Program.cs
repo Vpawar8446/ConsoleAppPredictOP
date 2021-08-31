3fcc0d0d4d94007bed20d43e4842bae21e15932a
niles <niles@LAPTOP-3K24CQ1R>
2021-08-31 20:30:51 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace ConsoleAppPredictOP
+{
+    enum MeetingImportance
+    {
+        Trivial,
+        Regular,
+        Critical,
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MeetingImportance meet = MeetingImportance.Critical;
+            int value = (int)MeetingImportance.Critical;
+
+            if (meet == MeetingImportance.Critical)
+            {
+                Console.WriteLine("Regular : " + value);
+
+            }
+            else if (meet == MeetingImportance.Regular)
+            {
+                Console.WriteLine("Critical : " + value);
+            }
+
+            else if (meet == MeetingImportance.Trivial)
+            {
+                Console.WriteLine("Trivial : " + value);
+            }
+
+        }
+    }
+}
